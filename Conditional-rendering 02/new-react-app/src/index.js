de0c58ef4110c9e13d941ea0e92969a79416a2f7
domedema <116555350+domedema@users.noreply.github.com>
2023-02-09 18:06:10 +0100

    Add files via upload

---
+++ Conditional-rendering 02/new-react-app/src/index.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+class Welcome extends React.Component {
+  static defaultProps = {
+    name: 'Guest',
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Welcome, {this.props.name}!</p>
+        {this.props.age && <p>Your age is {this.props.age}</p>}
+
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return <Welcome name="John" age={30} />;
+  }
+}
