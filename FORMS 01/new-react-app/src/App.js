7be282d35702a6722c40e0bee6748e21befbbc80
domedema <116555350+domedema@users.noreply.github.com>
2023-02-22 12:31:31 +0100

    Add files via upload

---
+++ FORMS 01/new-react-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import InteractiveWelcome from './InteractiveWelcome';
+import Welcome from './Welcome';
+
+
+export default class App extends React.Component {
+  render() {
+    return <InteractiveWelcome></InteractiveWelcome>;
+
+  }
+}
