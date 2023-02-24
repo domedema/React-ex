09e644d90b6077e37bc1d00dfea71d17df3cd65e
domedema <116555350+domedema@users.noreply.github.com>
2023-02-24 10:58:26 +0100

    Add files via upload

---
+++ COMPONENT COMPOSITION 01/new-react-app/src/Container.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default class Container extends React.Component {
+    render() {
+        return (
+            <div className="bg-white border border-red-500 p-4">
+                <Container>
+                    <h1>Hello World!</h1>
+                    <p>This is some text inside a container.</p>
+                </Container>
+            </div>
+        )
+    }
+}
