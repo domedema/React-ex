815de18af187c4e753243f793dda042a471a444f
domedema <116555350+domedema@users.noreply.github.com>
2023-03-14 18:02:21 +0100

    Add files via upload

---
+++ EVENTS 03/src/App.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import ClickTracker from './ClickTracker';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <ClickTracker />
+            </div>
+        )
+    }
+}
+
+export default App;
